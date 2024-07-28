349e1d2608ed17b4b66c532f71167b13a102e610
kinomagnum <kinomagnum@gmail.com>
2024-07-28 13:32:32 +0300

    вdey2Commit

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
 
+        int floorCount = scaner.nextInt();
+        if (floorCount >= 1 && floorCount <= 4) {
+            System.out.println("Мало этажный дом ");
+        } else if (floorCount >= 5 && floorCount <= 8) {
+            System.out.println("Средне этажный дом ");
+        } else if (floorCount >= 9) {
+            System.out.println("Много этажный дом");
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
