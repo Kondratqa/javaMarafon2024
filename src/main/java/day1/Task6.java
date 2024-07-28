5de93fef6166efbbf088f586b4b9450d05ae23fd
kinomagnum <kinomagnum@gmail.com>
2024-07-28 12:53:56 +0300

    initialCommit

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,29 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int a = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " * " + a + " = " + i * a);
+            System.out.println("_");
+        }
+        char b = '_';
+        for (int i = 0; i < 1; i++) {
+            System.out.println(b);
+        }
+        int c = 8;
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i + " * " + c + " = " + i * c);
+            System.out.println("_");
+        }
+        char d = '_';
+        for (int i = 0; i < 1; i++) {
+            System.out.println(d);
+        }
+        int e = 7;
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i + " * " + e + " = " + i * e);
+            System.out.println("_");
+        }
+    }
+}
